7f4bd28b9ca2f1392f98b51a02e9b83f691f1cbf
Joey van Noort <joey@lvannoort.com>
2025-03-15 18:16:57 +0100

    Refactor service command execution to include full path for systemctl

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify, request, render_template, send_from_directory
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
 import subprocess
 
-
 app = Flask(__name__, static_folder='dist', static_url_path='')
-CORS(app, resources={r"/*": {"origins": "*"}})  # Allow specific origin
-print("Allowed origins:", "*")
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-SERVICES = ["pikaraoke-ledfx", "spotify-ledfx"]  # Define services
+SERVICES = ["pikaraoke-ledfx", "spotify-ledfx"]
 
 def run_command(command):
     """Run a shell command and return output"""
@@ -17,7 +15,6 @@ def run_command(command):
     except subprocess.CalledProcessError as e:
         return e.stderr.strip()
 
-
 @app.route("/")
 def index():
     return send_from_directory(app.static_folder, 'index.html')
@@ -26,7 +23,7 @@ def index():
 def start_service(service):
     if service not in SERVICES:
         return jsonify({"error": "Invalid service"}), 400
-    output = run_command(f"sudo systemctl start {service}.service")
+    output = run_command(f"sudo /bin/systemctl start {service}.service")
     response = jsonify({"message": f"{service} started", "output": output})
     return response
 
@@ -34,16 +31,15 @@ def start_service(service):
 def stop_service(service):
     if service not in SERVICES:
         return jsonify({"error": "Invalid service"}), 400
-    output = run_command(f"sudo systemctl stop {service}")
+    output = run_command(f"sudo /bin/systemctl stop {service}.service")
     response = jsonify({"message": f"{service} stopped", "output": output})
     return response
-
 
 @app.route("/status/<service>", methods=["GET"])
 def status_service(service):
     if service not in SERVICES:
         return jsonify({"error": "Invalid service"}), 400
-    command = f"systemctl is-active {service}.service"
+    command = f"/bin/systemctl is-active {service}.service"
     output = run_command(command)
     if output == "active":
         output
@@ -51,7 +47,6 @@ def status_service(service):
         output = "inactive"
     response = jsonify({"service": service, "status": output})
     return response
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
